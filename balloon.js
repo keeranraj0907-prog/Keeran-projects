bb20d81f192c96860c3b433ef00d266001a088de
Keeran <keeranraj123@gmail.com>
2026-05-10 19:36:38 +0800

    Update balloon.js

--- balloon.js
+++ balloon.js
@@ -9,3 +9,24 @@ function updateBalloon()  {
     balloon.style.height = size+″px″;
     balloon.style.backgroundColor = color(colorIndex);
 }
+
+balloon.addEventListener(″click″ , () => {
+    size +=10;
+    colorIndex = (colorIndex+1 ) %color.length;
+   
+    if(size>420) {
+        size=200:
+    
+    }
+
+    updateBalloon();
+});
+
+balloon.addEventListener(″mouseleave″ ,() => {
+   size -=5;
+   if(size<200) size=200;
+
+   colorIndex = {colorIndex-1 +color.length) %color.length;
+   
+   updateBalloon();
+});
