3a0760be5849ed4970fa8b6bea2472a964c6bec1
Keeran <keeranraj123@gmail.com>
2026-05-10 17:45:09 +0800

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,17 @@
+function myFunction() {
+
+const randomColor= Math.floor(Math.random() *16777215).toString
+(16);
+
+const paddedColor= "#"+ randomColor.padStart(6,ʼ0ʼ);
+document.body.style.backgroundColor = paddedColor;
+
+}
+
+// button.addEventListener(ʼclickʼ, function() {
+
+//button.style.backgroundColor =getRandomColor();
+
+// }) ;
+
 
