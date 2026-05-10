798a2ed508dbb660575c98bd308ad33ff7c5fa9c
Keeran <keeranraj123@gmail.com>
2026-05-10 19:23:44 +0800

    Update balloon.js

--- balloon.js
+++ balloon.js
@@ -1,1 +1,11 @@
+const balloon = document.getElementById(″balloon″)
 
+let size=200;
+let color= {″red″,″green″,″blue″};
+let colorIndex= 0;
+
+function updateBalloon()  {
+    balloon.style.width  = size+″px″;
+    balloon.style.height = size+″px″;
+    balloon.style.backgroundColor = color(colorIndex);
+}
